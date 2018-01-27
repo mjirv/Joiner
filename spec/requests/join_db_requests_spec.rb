ea52813952e3fbf0133fa94eeadd63c3d4086b3b
Michael Irvine <michael.j.irvine@gmail.com>
2018-01-27 10:19:06 -0800

    View JoinDb tests pass

--- spec/requests/join_db_requests_spec.rb
+++ spec/requests/join_db_requests_spec.rb
@@ -4,12 +4,46 @@ require 'rails_helper'
 
 describe "Viewing a JoinDb", type: :request do
     it "denies access if you're not logged in" do
+        user_attributes = FactoryBot.attributes_for(:user)
+        user = User.create!(user_attributes)
+
+        join_db_attributes = FactoryBot.attributes_for(:join_db)
+        join_db_attributes[:user_id] = user.id
+        join_db = JoinDb.create!(join_db_attributes)
+
+        get join_db_url(join_db.id)
+        expect(response).to redirect_to('/signup')
     end
 
     it "denies access if you're the wrong user" do
+        user_attributes = FactoryBot.attributes_for(:user)
+        user = User.create!(user_attributes)
+
+        join_db_attributes = FactoryBot.attributes_for(:join_db)
+        join_db_attributes[:user_id] = user.id
+        join_db = JoinDb.create!(join_db_attributes)
+
+        new_user_attributes = FactoryBot.attributes_for(:user)
+        User.create!(new_user_attributes)
+
+        post "/login", params: new_user_attributes
+
+        get join_db_url(join_db.id)
+        expect(response).to redirect_to('/')
     end
 
     it "succeeds if you're logged in and the right user" do
+        user_attributes = FactoryBot.attributes_for(:user)
+        user = User.create!(user_attributes)
+
+        join_db_attributes = FactoryBot.attributes_for(:join_db)
+        join_db_attributes[:user_id] = user.id
+        join_db = JoinDb.create!(join_db_attributes)
+
+        post "/login", params: user_attributes
+
+        get join_db_url(join_db.id)
+        expect(response).to have_http_status(200)
     end
 end
 
