99893f5260dd37cb4ba8cf7b8d97f8ef9e4558b0
Michael Irvine <michael.j.irvine@gmail.com>
2018-02-15 09:06:13 -0800

    First pass at adding redshift

--- app/models/remote_db.rb
+++ app/models/remote_db.rb
@@ -1,11 +1,16 @@
 class RemoteDb < ApplicationRecord
   belongs_to :join_db
-  enum db_type: %w(postgres mysql sql_server)
+  enum db_type: %w(postgres mysql sql_server redshift)
   validates :join_db_id, presence: true
   validates :db_type, presence: true
   validates :database_name, presence: true
   validates :remote_user, presence: true
   validates :port, presence: true
   validates :host, presence: true
-  validates :schema, presence: true, if: :postgres? 
+  validates :schema, presence: true, if: needs_schema?
+
+  private
+  def needs_schema?
+    self.postgres? or self.redshift?
+  end
 end
